71720f1283a6ed41bcc7f8f0db8acef6af5fcf67
David Robertson <david.robertson012@gmail.com>
2015-01-27 11:45:34 +0000

    first commit

---
+++ tango_with_django_project/rango/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+def index(request): # index is name of view, (request) is arguement
+    context_dict = {'boldmessage': "I am bold font from the context"}
+
+
+    return render(request, 'rango/index.html', context_dict)
+
+def rango/about(request):
+    context_dict = {'boldmessage': "This tutorial has been put together by David Robertson, 1106602R"}
+
+    return render(request, 'rango/about.html', context_dict)
